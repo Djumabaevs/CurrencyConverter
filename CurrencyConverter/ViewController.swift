3b9f60d8b834caf0ce34b94c2c2720fe4b8a91e4
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-31 22:37:29 +0600

    add button and suggested constraints

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kztLabel: UILabel!
+    @IBOutlet weak var rubLabel: UILabel!
+    @IBOutlet weak var eurLabel: UILabel!
+    @IBOutlet weak var btcLabel: UILabel!
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

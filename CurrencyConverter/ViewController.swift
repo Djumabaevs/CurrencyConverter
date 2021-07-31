9987baa609ffffeb5a37eb9ec7161d64d00fb919
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-31 22:53:13 +0600

    alert controller and alert action

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -36,6 +36,11 @@ class ViewController: UIViewController {
         
         let task = session.dataTask(with: url!) { (data, response, error) in
             
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                
+            }
         }
     }
 }

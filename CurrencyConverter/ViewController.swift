3a31bbbae495557ce9e32c885fe4815207557b2e
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-31 22:40:35 +0600

    three steps to start get data from net

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -16,11 +16,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var cadLabel: UILabel!
     @IBOutlet weak var tryLabel: UILabel!
     @IBOutlet weak var usdLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
 
 
+    @IBAction func getRatesClicked(_ sender: Any) {
+        
+        //Request and session
+        //Response and Data
+        //Parsing and JSON serialization
+        
+        
+    }
 }
 
